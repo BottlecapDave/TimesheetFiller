b0f292a886800c8b60dd19321dc6df09901f5f09
BottlecapDave <me@davidskendall.co.uk>
2022-07-23 20:36:46 +0100

    build: Updated to .NET 6

--- src/TimesheetFiller.Cli/Options.cs
+++ src/TimesheetFiller.Cli/Options.cs
@@ -1,16 +1,15 @@
 ﻿using CommandLine;
 
-namespace TimesheetFiller.Cli
+namespace TimesheetFiller.Cli;
+
+public class Options
 {
-    public class Options
-    {
-        [Option('i', "input", Required = true, HelpText = "The input path of the config")]
-        public string Input { get; set; }
+    [Option('i', "input", Required = true, HelpText = "The input path of the config")]
+    public string Input { get; set; }
 
-        [Option("offset", HelpText = "The number of days to offset from today. For example, if you set this to 1, then it will run for yesterday and today.")]
-        public int? DaysToOffset { get; set; }
+    [Option("offset", HelpText = "The number of days to offset from today. For example, if you set this to 1, then it will run for yesterday and today.")]
+    public int? DaysToOffset { get; set; }
 
-        [Option("dry-run", HelpText = "Determines if this is is run in dry run")]
-        public bool IsDryRun { get; set; }
-    }
+    [Option("dry-run", HelpText = "Determines if this is is run in dry run")]
+    public bool IsDryRun { get; set; }
 }
--- src/TimesheetFiller.Cli/Program.cs
+++ src/TimesheetFiller.Cli/Program.cs
@@ -1,74 +1,50 @@
-﻿using CommandLine;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Logging;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using TimesheetFiller.GoogleCalendar;
 using TimesheetFiller.Harvest;
+using CommandLine;
+using TimesheetFiller.Cli;
 using YamlDotNet.Serialization;
+using Google.Apis.Auth.OAuth2;
 using YamlDotNet.Serialization.NamingConventions;
 
-namespace TimesheetFiller.Cli
-{
-    class Logger : Core.ILogger
-    {
-        public Task LogAsync(string msg)
+Parser.Default.ParseArguments<Options>(args)
+    .WithNotParsed<Options>(errors => {
+        System.Console.WriteLine("Error");
+    })
+    .WithParsedAsync<Options>(async options => {
+        using (var reader = new StreamReader(options.Input))
         {
-            Console.WriteLine(msg);
-            return Task.FromResult(false);
+            var content = await reader.ReadToEndAsync();
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            //yml contains a string containing your YAML
+            var config = deserializer.Deserialize<Config>(content);
+
+            var from = DateTime.Now.Date.AddDays((options.DaysToOffset ?? 0) * -1).Date;
+            var to = DateTime.Now.Date.AddDays(1).AddSeconds(-1);
+
+            UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets()
+                {
+                    ClientId = config.Google.ClientId,
+                    ClientSecret = config.Google.ClientSecret
+                },
+                new[] { "https://www.googleapis.com/auth/calendar.events.readonly", "https://www.googleapis.com/auth/calendar.readonly" },
+                "user",
+                CancellationToken.None
+            );
+
+            var logger = new Logger();
+            var calendarService = new GoogleCalendarService(credential, config.Google, logger);
+            var timesheetService = new HarvestTimesheetService(config.Harvest, logger);
+
+            var events = await calendarService.GetEventsAsync(from, to);
+            await timesheetService.AddTimesheetEntries(from, to, events, options.IsDryRun);
         }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed<Options>(o => new Program().Run(o).Wait())
-                .WithNotParsed<Options>(errors => {
-                    System.Console.WriteLine("Error");
-                });
-        }
-
-        private async Task Run(Options options)
-        {
-            using (var reader = new StreamReader(options.Input))
-            {
-                var content = await reader.ReadToEndAsync();
-
-                var deserializer = new DeserializerBuilder()
-                    .WithNamingConvention(UnderscoredNamingConvention.Instance)
-                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                    .Build();
-
-                //yml contains a string containing your YAML
-                var config = deserializer.Deserialize<Config>(content);
-
-                var from = DateTime.Now.Date.AddDays((options.DaysToOffset ?? 0) * -1).Date;
-                var to = DateTime.Now.Date.AddDays(1).AddSeconds(-1);
-
-                UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    new ClientSecrets()
-                    {
-                        ClientId = config.Google.ClientId,
-                        ClientSecret = config.Google.ClientSecret
-                    },
-                    new[] { "https://www.googleapis.com/auth/calendar.events.readonly", "https://www.googleapis.com/auth/calendar.readonly" },
-                    "user",
-                    CancellationToken.None
-                );
-
-                var logger = new Logger();
-                var calendarService = new GoogleCalendarService(credential, config.Google, logger);
-                var timesheetService = new HarvestTimesheetService(config.Harvest, logger);
-
-                var events = await calendarService.GetEventsAsync(from, to);
-                await timesheetService.AddTimesheetEntries(from, to, events, options.IsDryRun);
-            }
-        }
-
-    }
-}
+    }).Wait();
